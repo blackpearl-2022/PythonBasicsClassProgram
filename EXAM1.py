2801ac51f103639936bec6f978c4a3fdd7d0394e
blackpearl-2022 <itheeshkt@gmail.com>
2023-01-20 22:10:58 +0530

    test2

---
+++ EXAM1.py
@@ -0,0 +1,31 @@
+#1.Print First 10 natural numbers using while loop
+# num=0
+# while (num<10):
+#     sum=num+1
+#     print (sum)
+#     num=num+1
+
+#Count the total number of digits in a number using while loop
+# num=int(input("Enter the number: ")) #123
+# count=0
+# while num!=0: #123!=0 | 12!=0 | 1!=0 | 0!=0 - false
+#     num//=10 # num= 123//10 = 12| num= 12//10=1 | num= 1//10 = 0
+#     count+=1 # count=0+1=1 | count=1+1=2 | count=2+1=3
+# print(count) # print count = 3
+
+#Display numbers from -10 to -1 using for loop
+# for i in range (-10,0):     # i=-10         | i=-9
+#     print(i)                # print -10     | i=-9
+#                             # i=-10+1=-9    | i=-9+1
+
+#Write a program to display all prime numbers within a range
+
+# lower=int(input("enter the lower limit: "))
+# upper=int(input("Enter the upper limnit: "))
+#
+# for i in range (lower,upper):
+#     if (i%)
+
+
+
+#.Display Fibonacci series up to 10 terms using for loop
